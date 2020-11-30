8d7b8283a5a139c9fdd538c1ffa89b0ead59e539
jagoba05 <jml_1993@hotmail.es>
2020-11-30 10:31:08 +0100

    Ejercicio6
    Codigo del ejercicio6

---
+++ practica2.1/Ejercicio6.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/utsname.h>
+
+int main(int argc,char ** argv){
+        printf("Longitud Máxima de Argumentos: %li\n", sysconf(_SC_ARG_MAX));
+        printf("Número máximo de hijos: %li\n", sysconf(_SC_CHILD_MAX));
+        printf("Número máximo de ficheros: %li\n", sysconf(_SC_OPEN_MAX));
+        return 0;
+}
