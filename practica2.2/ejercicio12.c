78e07138327ac7f0a9887087a5098ce16318f7a9
jagoba05 <jml_1993@hotmail.es>
2020-12-07 17:08:05 +0100

    Ejercicio12 y imagen
    Codigo del ejercicio 12 y  foto de la salida redirigida al archivo

---
+++ practica2.2/ejercicio12.c
@@ -0,0 +1,21 @@
+include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc,char **argv){
+    if(argc < 2){
+        printf("Falta la ruta del archivo\n");
+        return -1;
+    }
+	int fd = open(argv[1], O_CREAT | O_RDWR,0777);
+    if (fd == -1){
+        printf("No se ha podido crear el fichero\n");
+        return -1;
+    }
+	int fd2 = dup2(fd,1);
+    printf("Se esta redirigiendo a %d\n",argv[1]);
+    dup2(fd2,fd);
+    return 1;
+}
